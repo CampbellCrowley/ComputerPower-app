3805e15d80b67c22fc0f8a354d65ab03320e706f
Campbell Crowley <github@campbellcrowley.com>
2021-04-26 17:46:32 -0700

    Added Graph, left drawer, power-state buttons
    Still need to make these function, but layout has begun.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,13 @@ void main() {
 
 void signOut(BuildContext context) async {
   await FirebaseAuth.instance.signOut();
-  Navigator.popUntil(context, (route) => route.isFirst);
+  Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => MyHomePage()));
+}
+
+Future<bool> sendCommand(String cmd) async {
+  // TODO: Implement sending command to server.
+  return false;
 }
 
 class MyApp extends StatelessWidget {
@@ -24,15 +30,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Computer Power Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  final String title = 'Computer Power Home Page';
 
   @override
   State<StatefulWidget> createState() => _HomePage();
@@ -42,14 +46,16 @@ class _HomePage extends State<MyHomePage> {
   bool _initialized = false;
   bool _error = false;
   bool _loading = false;
-  User user;
 
-  void initilizeFlutterFire() async {
+  void initializeFlutterFire() async {
     try {
       await Firebase.initializeApp();
       setState(() => _initialized = true);
       FirebaseAuth.instance.authStateChanges().listen((User myUser) {
-        setState(() => user = myUser);
+        if (myUser != null) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => _MainView(myUser)));
+        }
       });
     } catch (e) {
       setState(() => _error = true);
@@ -59,7 +65,7 @@ class _HomePage extends State<MyHomePage> {
 
   @override
   void initState() {
-    initilizeFlutterFire();
+    initializeFlutterFire();
     super.initState();
   }
 
@@ -91,11 +97,7 @@ class _HomePage extends State<MyHomePage> {
       );
     }
 
-    if (user == null) {
-      return _SignInView();
-    } else {
-      return _MainView(user);
-    }
+    return _SignInView();
   }
 }
 
@@ -141,16 +143,123 @@ class _SignInView extends StatelessWidget {
   }
 }
 
-class _MainView extends StatelessWidget {
+class _MainView extends StatefulWidget {
   _MainView(this.user);
 
   final User user;
 
   @override
+  State<StatefulWidget> createState() => _MainState();
+}
+
+class _MainState extends State<_MainView> {
+  _MainState();
+
+  int selectedComputer = 0;
+
+  // TODO: Temporary example data until I fetch from the API.
+  final List<Map> computerData = [
+    {
+      "id": 0,
+      "name": 'My Desktop #1',
+      "currentState": "On",
+      "previousStateChange": "Mon 04-26 07:56:16",
+      "now": "Mon Apr 26 2021 14:37:07 GMT-0800 (AKDT)",
+      "history": <double>[55.32, 62.74, 61.84, 60.12, 69.36, 56.75, 67.28]
+    },
+    {
+      "id": 1,
+      "name": 'My Desktop #2',
+      "currentState": "On",
+      "previousStateChange": "Mon 04-26 07:56:16",
+      "now": "Mon Apr 26 2021 14:37:07 GMT-0800 (AKDT)",
+      "history": <double>[55.32, 62.74, 61.84, 60.12, 69.36, 56.75, 67.28]
+    },
+    {
+      "id": 2,
+      "name": 'My Desktop #3',
+      "currentState": "On",
+      "previousStateChange": "Mon 04-26 07:56:16",
+      "now": "Mon Apr 26 2021 14:37:07 GMT-0800 (AKDT)",
+      "history": <double>[55.32, 62.74, 61.84, 60.12, 69.36, 56.75, 67.28]
+    }
+  ];
+
+  @override
+  void initState() {
+    if (widget.user == null) {
+      Future(() => Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyHomePage())));
+    }
+    super.initState();
+  }
+
+  void selectComputer(BuildContext context, int index) {
+    setState(() {
+      selectedComputer = index;
+    });
+    Navigator.pop(context);
+  }
+
+  List<Widget> getDrawerChildren(BuildContext context) {
+    List<Widget> drawer = [
+      DrawerHeader(
+        child: Text('${widget.user.displayName}\'s Computers',
+            style: Theme.of(context).textTheme.headline4),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          image: DecorationImage(
+              image: NetworkImage(widget.user.photoURL), fit: BoxFit.cover),
+        ),
+      ),
+    ];
+    for (int i = 0; i < computerData.length; i++) {
+      Map el = computerData[i];
+      drawer.add(
+        ListTile(
+          title: Text(el["name"]),
+          onTap: () => selectComputer(context, i),
+          tileColor: i == selectedComputer ? Colors.lightBlue : Colors.white,
+        ),
+      );
+    }
+    return drawer;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (widget.user == null) {
+      return Text("Unknown user...");
+    }
+    Map comp = computerData[selectedComputer];
+    List<MapEntry<int, double>> data = comp["history"].asMap().entries.toList();
+    // print(data);
+    List<Series<MapEntry, String>> seriesList = [
+      new Series<MapEntry, String>(
+          id: 'Power Percentage',
+          domainFn: (MapEntry el, _) {
+            final List<String> days = [
+              "Sun",
+              "Mon",
+              "Tue",
+              "Wed",
+              "Thu",
+              "Fri",
+              "Sat"
+            ];
+            // print('Domain: ${el}: ${days[el.key]}');
+            return '${days[el.key]}';
+          },
+          measureFn: (MapEntry el, _) {
+            // print('Measure: ${el.value}');
+            return el.value;
+          },
+          data: data)
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main View'),
+        title: Text(comp["name"]),
         actions: [
           IconButton(
               onPressed: () {
@@ -164,18 +273,49 @@ class _MainView extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'Hello World!',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              height: 200.0,
+              child: BarChart(seriesList, animate: true),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: Text('Press Power'),
+                  onPressed: () => sendCommand('press-power'),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green)),
+                ),
+                ElevatedButton(
+                  child: Text('Hold Power'),
+                  onPressed: () => sendCommand('hold-power'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.lightGreen)),
+                ),
+                ElevatedButton(
+                  child: Text('Press Reset'),
+                  onPressed: () => sendCommand('press-reset'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.orange)),
+                ),
+              ],
             ),
             Text(
-              'Welcome ${user.displayName}!',
+              'Current State: ${comp["currentState"]}',
+              style: Theme.of(context).textTheme.subtitle1,
             ),
           ],
         ),
       ),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: getDrawerChildren(context),
+      )),
     );
   }
 }
